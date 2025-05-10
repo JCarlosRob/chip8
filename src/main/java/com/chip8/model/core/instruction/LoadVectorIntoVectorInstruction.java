d889f04d8a769dc1c2ebc3b8b644739f7d4c6176
Juan Carlos Robles <carlos.jcr@hotmail.com>
2025-05-10 13:45:24 +0200

    fix 8xy0 instruction

--- src/main/java/com/chip8/model/core/instruction/LoadVectorIntoVectorInstruction.java
+++ src/main/java/com/chip8/model/core/instruction/LoadVectorIntoVectorInstruction.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HexFormat;
-import java.util.Objects;
 
 @Component
 public class LoadVectorIntoVectorInstruction extends InstructionAbstract {
@@ -23,10 +22,7 @@ public class LoadVectorIntoVectorInstruction extends InstructionAbstract {
     @Override
     public void execute(final String data) {
         final Integer positionVx = HexFormat.fromHexDigits(data.substring(1, 2));
-        final Integer vx = this.vRegister.get(positionVx);
-        final Integer vy = this.vRegister.get(HexFormat.fromHexDigits(data.substring(2, 3)));
-        if (Objects.equals(vx, vy)) {
-            this.vRegister.set(positionVx, vy);
-        }
+        final Integer vyValue = this.vRegister.get(HexFormat.fromHexDigits(data.substring(2, 3)));
+        this.vRegister.set(positionVx, vyValue);
     }
 }
